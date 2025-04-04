7bf815ea4be98108c78d4b616b3e3453ed33daa3
Sim0Batt <simonebatt51@gmail.com>
2025-04-04 10:12:19 +0200

    Rimozione ProjectWidget inutilizzato

---
+++ src/components/ProjectWidget.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-btn @click="fetchData">{{ message }}</v-btn>
+  </div>
+</template>
+
+<script>
+import { fetchData } from '@/api/api'
+
+export default {
+  data() {
+    return { message: 'ApiCall' }
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await fetchData()
+        this.message = response || 'No message received'
+        console.log('projects: ' + this.message)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+        this.message = 'Error loading data'
+      }
+    },
+  },
+}
+</script>
